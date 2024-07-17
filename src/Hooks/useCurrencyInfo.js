11bb4d4d7c4791f21f3f5735e83b19c422150ad5
vizzRG <vizzrg@gmail.com>
2024-07-17 13:04:05 +0530

    Finalised

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -6,11 +6,10 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency])) 
-        console.log(data)
+       
         
     }, [currency])
 
-    console.log(data)
     return data
 }
 
